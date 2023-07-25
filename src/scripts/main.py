be46945f1cd7ed0ce10ddcfe2de992c745191c07
idipanshu <dipanshu@wednesday.is>
2023-07-25 13:38:55 +0530

    feat: add a sample script

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,18 +1,11 @@
-"""
-Getting started with PySpark
-"""
-
 from pyspark.sql.session import SparkSession
+import pyspark.sql.functions as F
 
 sc = SparkSession.builder.appName("wednesday").getOrCreate()
 
-columns = ["name", "email", "role"]
-data = [
-    ("James", "james@gmail.com", "Developer"),
-    ("John", "john@gmail.com", "Developer"),
-]
+df = sc.read.csv("src/data/survey_results_public.csv", header=True, inferSchema=True)
 
-df = sc.createDataFrame(data=data, schema=columns)
+# Filter records where DevType contains "Developer, front-end"
+df = df.filter(F.col("DevType").contacins("Developer, front-end"))
 
-df.printSchema()
 df.show()
