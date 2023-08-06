4fb16a708dc464313fe4ede214a39a6f62d0e2e4
idipanshu <dipanshu@wednesday.is>
2023-08-07 01:31:56 +0530

    feat: add glue deployment CD

--- automation/deploy_glue_job.sh
+++ automation/deploy_glue_job.sh
@@ -1,18 +1,38 @@
 #! /bin/bash
 
+# First argument is the S3 path to the scripts
+s3_path="$1"
+
 # Loop over all files in src/scripts/
-for file in src/scripts/*.py
+for directory in src/scripts/*;
 do
-    # Get the filename without the extension
-    filename=$(basename -- "$file")
-    filename="${filename%.*}"
+    # Extract the filename from the directory path
+    dir_path="${directory%/}"
+    file_name="${dir_path##*/}"
 
-    s3_path="s3://sample-glue-wednesday/scripts"
+    if [ -e "$dir_path/main.py" ]; then
+      # Check if the job exists, if not create it, else update it
+      job_exists=$(aws glue get-job --job-name $file_name | jq -r .Job.Name)
 
-    # Set the script name in the glue_job.json file using jq
-    json_data=$(jq --arg SCRIPT_NAME "$filename" '.JobName=$SCRIPT_NAME' automation/update_glue_job.json)
-    json_data=$(jq --arg LOCATION "$s3_path/$filename.py" '.JobUpdate.Command.ScriptLocation=$LOCATION' <<< $json_data)
+      if [ "$job_exists" != "$file_name" ]; then
+        # Set the script name in the glue_job.json file using jq
+        json_data=$(jq --arg SCRIPT_NAME "$file_name" '.Name=$SCRIPT_NAME' automation/create_glue_job.json)
+        json_data=$(jq --arg LOCATION "$s3_path/$file_name.py" '.Command.ScriptLocation=$LOCATION' <<< $json_data)
 
-    # Update the glue job
-    aws glue update-job --cli-input-json "$json_data" --profile default
+        # Create the glue job
+        echo "Creating glue job $file_name"
+        aws glue create-job --cli-input-json "$json_data"
+      else
+        # Set the script name in the glue_job.json file using jq
+        json_data=$(jq --arg SCRIPT_NAME "$file_name" '.JobName=$SCRIPT_NAME' automation/update_glue_job.json)
+        json_data=$(jq --arg LOCATION "$s3_path/$file_name.py" '.JobUpdate.Command.ScriptLocation=$LOCATION' <<< $json_data)
+
+        # Update the glue job
+        echo -e "\nUpdating glue job $file_name"
+        aws glue update-job --cli-input-json "$json_data"
+      fi
+
+    else
+      echo "main.py file does not exist in $dir_path."
+    fi
 done
---
+++ src/scripts/01_pyspark_in_glue_jobs/main.py
@@ -0,0 +1,33 @@
+# import sys
+from awsglue.job import Job
+from awsglue.context import GlueContext
+
+# from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+
+# from pyspark.sql.session import SparkSession
+import pyspark.sql.functions as f
+
+# Spark context setup
+sc = SparkContext()
+
+# Glue context setup
+glue_context = GlueContext(sc)
+
+# Spark session setup
+spark_session = glue_context.spark_session
+
+# Initialize glue job
+job = Job(glue_context)
+job.init("sample")
+
+df = spark_session.read.csv(
+    "src/data/survey_results_public.csv", header=True, inferSchema=True
+)
+
+df = df.filter(f.col("Country").eqNullSafe("Australia"))
+df = df.filter(f.col("OpSysPersonal use").eqNullSafe("macOS"))
+
+df.select("Country", "OpSysPersonal use").show()
+
+job.commit()
